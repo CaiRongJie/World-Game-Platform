2d0d2621da0de2cad9426e113fd7ad3a44a301a6
CaiRongJie <2054410755@qq.com>
2019-05-10 13:10:21 +0800

    Add project

---
+++ World Game Platform/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace World_Game_Platform.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
