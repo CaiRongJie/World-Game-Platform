bd90b3f7649fad0dbc806bf24acc64a90c1b7c5c
CaiRongJie <2054410755@qq.com>
2019-05-31 11:48:05 +0800

    Add project

---
+++ World Game Platform/Models/G.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace World_Game_Platform.Models
+{
+    public class G
+{
+        public int GameId { get; set; }           // example 23
+        public string GameName { get; set; }       // LeBron
+
+        public string PlaftormName { get; set; }        // Lakers
+        public P P { get; set; }
+
+    }
+}
---
+++ World Game Platform/Models/P.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace World_Game_Platform.Models
+{
+    public class P
+{
+        [Key]
+        [MaxLength(30)]
+        public string GameName { get; set; }
+
+        [MaxLength(30)]
+        public string Country { get; set; }
+
+        public List<P> Game { get; set; }
+    }
+}
